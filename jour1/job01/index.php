a3024fd377b128810e2331e1e14d065d9065048a
Elidnaya <romain.massa@laplateforme.io>
2025-01-13 12:29:44 +0100

    jour1job01

--- jour1/job01/index.php
+++ jour1/job01/index.php
@@ -1,2 +1,14 @@
 <?php
 echo"Hello LaPlateforme";
+$str=" LaPlateforme";
+$str2=" Vive";
+$str3=" !";
+echo($str2 . $str . $str3);
+$val=" 6";
+echo($val);
+$val + 4;
+echo( $val + 4);
+$mybool=" true";
+echo($mybool);
+$mybool=" false";
+echo($mybool);
